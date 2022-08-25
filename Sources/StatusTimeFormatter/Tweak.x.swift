18ed7cbd2ebdd3002b38a88019e76223f6d81322
Matt Anisovich <anisovich.matthew@gmail.com>
2022-08-25 14:32:50 +0100

    Initial commit

--- Sources/StatusTimeFormatter/Tweak.x.swift
+++ Sources/StatusTimeFormatter/Tweak.x.swift
@@ -2,13 +2,26 @@ import Orion
 import StatusTimeFormatterC
 
 class StatusBarTimeHook: ClassHook<_UIStatusBarStringView> {
+    // orion:new
+    func isDateLabel(_ text: String) -> Bool {
+        return text.contains(":")
+    }
+
     func setText(_ text: String) {
-        if text.contains(":") {
+        if isDateLabel(text) {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyy-MM-dd HH:mm"
             orig.setText(formatter.string(from: Date()))
         } else {
             orig.setText(text)
+        }
+    }
+
+    func setTextColor(_ color: UIColor) {
+        if isDateLabel(target.text ?? "") {
+            orig.setTextColor(.blue)
+        } else {
+            orig.setTextColor(color)
         }
     }
 }
